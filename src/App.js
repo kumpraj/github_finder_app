72161794cb40fa2df2357bac27f9fedc72b91bbe
Prajwal Kumar <kumpraj@gmail.com>
2023-01-31 16:26:14 +0530

    added home,about & notfound routes/pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import About from "./Pages/About";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col justify-between h-screen text-neutral-content">
+      <div className="flex flex-col justify-between h-screen">
         <Navbar />
 
-        <main className="container mx-auto">
-          Content
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element= {<Home/>} />
+            <Route path="/about" element= {<About/>}/>
+            <Route path="/notfound" element= {<NotFound/>} />
+            <Route path="*" element= {<NotFound/>} />
+          </Routes>
         </main>
 
         <Footer />
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>
+        <h1 className="text-6xl text-center">Welcome</h1>
+    </div>
+  )
+}
+
+export default Home
