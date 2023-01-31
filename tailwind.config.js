870c0e625800b57d27c459b421e4789397100e84
Prajwal Kumar <kumpraj@gmail.com>
2023-01-31 13:12:19 +0530

    added Navbar component

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,17 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
+
+  /* // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+    default: "dark"
+  }, */
 }
