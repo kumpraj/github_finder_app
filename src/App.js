8cb07021f341a40fd8f2b86963ccc95edb86694d
Prajwal Kumar <kumpraj@gmail.com>
2023-02-04 16:47:26 +0530

    getUser functionality defined

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Alert from "./components/layout/Alert";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
-import { AlertProvider } from "./context/alert/AlertContext";
-import { GithubProvider } from "./context/github/GithubContext";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
+import User from "./Pages/User";
 import NotFound from "./Pages/NotFound";
+import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
                     }
                 />
                 <Route path="/about" element= {<About/>}/>
+                <Route path="/user/:login" element= {<User/>}/>
                 <Route path="/notfound" element= {<NotFound/>} />
                 <Route path="*" element= {<NotFound/>} />
               </Routes>
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
 
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
@@ -40,6 +41,32 @@ export const GithubProvider = ({ children }) => {
         })
     }
 
+    //  get single user
+    const getUser = async (login) => {
+
+        setLoading();
+
+        
+        const response = await fetch(`${GITHUB_URL}/users/${login}`,{
+            headers: {
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        });
+
+        if(response.status === 404){
+            window.location('/notfound')
+        }else {
+            const data = await response.json();
+        
+            dispatch({
+                type: 'GET_USER',
+                payload: data
+            })
+        }
+
+        
+    }
+
     //  clear users from the state
     const clearUsers = () => dispatch({type: 'CLEAR_USERS'});
 
@@ -49,9 +76,11 @@ export const GithubProvider = ({ children }) => {
     return (
         <GithubContext.Provider value={{
             users: state.users,
+            user: state.user,
             loading: state.loading,
             searchUsers,
-            clearUsers
+            clearUsers,
+            getUser
         }}>
             { children }
         </GithubContext.Provider>
