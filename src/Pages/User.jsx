6247097ad355b6045dc939dfae79c74f9c175906
Prajwal Kumar <kumpraj@gmail.com>
2023-02-06 14:13:52 +0530

    modified actions and used axios

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -3,7 +3,7 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { Link, useParams } from "react-router-dom";
 import RepoList from "../components/repos/RepoList";
 import GithubContext from "../context/github/GithubContext"
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 
 function User() {
@@ -14,12 +14,12 @@ function User() {
 
     useEffect(() => {
         dispatch({type: 'SET_LOADING'});
+        
         const getUserData = async () => {
-            const userData = await getUser(params.login);
-            dispatch({type: 'GET_USER', payload: userData})
 
-            const userRepoData = await getUserRepos(params.login);
-            dispatch({type: 'GET_REPOS', payload: userRepoData})
+            const userData = await getUserAndRepos(params.login);
+            dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
+            
         }
 
         getUserData();
