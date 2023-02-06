a6ec42bd5603824442560a6f5b624d839f15f03d
Prajwal Kumar <kumpraj@gmail.com>
2023-02-06 13:09:06 +0530

    moved searchUsers to action file

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -19,28 +19,7 @@ export const GithubProvider = ({ children }) => {
 
     
 
-    //  get search results
-    const searchUsers = async (text) => {
-
-        setLoading();
-
-        const params = new URLSearchParams({
-            q: text
-        })
-
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,{
-            headers: {
-                Authorization: `token ${GITHUB_TOKEN}`
-            }
-        });
-
-        const { items } = await response.json();
-        
-        dispatch({
-            type: 'GET_USERS',
-            payload: items
-        })
-    }
+    
 
     //  get single user
     const getUser = async (login) => {
@@ -102,11 +81,8 @@ export const GithubProvider = ({ children }) => {
 
     return (
         <GithubContext.Provider value={{
-            users: state.users,
-            user: state.user,
-            repos: state.repos,
-            loading: state.loading,
-            searchUsers,
+            ...state,
+            dispatch,
             clearUsers,
             getUser,
             getUserRepos
